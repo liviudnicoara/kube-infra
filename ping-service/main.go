8b4b260510224335e8a700acfab6d9882906e09d
liviudnicoara <119079662+liviudnicoara@users.noreply.github.com>
2023-10-17 18:41:43 +0300

    created ping service

---
+++ ping-service/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		vars := os.Environ()
+		return c.HTML(http.StatusOK, "I am PING service <br/>"+fmt.Sprintf("%+v", vars))
+	})
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	})
+
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+
+	e.Logger.Fatal(e.Start(":" + httpPort))
+}
